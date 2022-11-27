a437887baa9598cfe07be6ea1b29aa3158756198
AnimewkaSes <brol0526@gmail.com>
2022-11-27 17:53:57 +0600

    Задание 4

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Программа для поиска наибольшего числа");
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int result = Math.Max(Math.Max( a, b), c);
+Console.WriteLine("Наибольшее число из трёх: "); 
+Console.WriteLine( result );
