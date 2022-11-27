7e3b715e1892d436aa068414b3fe5c0f83feca6c
AnimewkaSes <brol0526@gmail.com>
2022-11-27 18:25:48 +0600

    Задание 8

--- Ex004/Program.cs
+++ Ex004/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Программа для определения числа");
+Console.WriteLine("Введите количество итераций");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вывод: ");
+for (int i = 1; i < n+1; i++)
+{   
+if (i%2==0)
+{
+    Console.WriteLine(i);
+}
+}
+Console.ReadKey();
