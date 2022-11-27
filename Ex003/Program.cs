46836cbd5e163bdecc4695860fad313138e5c88c
AnimewkaSes <brol0526@gmail.com>
2022-11-27 18:13:19 +0600

    Задание 6

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Программа для определения числа");
+double number = Convert.ToDouble(Console.ReadLine());
+//double result = number/2;
+if (number%2==0)
+{
+    Console.WriteLine("Чётное");
+}else{
+    Console.WriteLine("Не чётное");
+}
+Console.ReadKey();
