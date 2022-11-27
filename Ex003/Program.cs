2f637a68a85d8b16a38da61a600a3c2f74dba703
AnimewkaSes <brol0526@gmail.com>
2022-11-27 18:26:41 +0600

    Задание 6

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -1,7 +1,6 @@
 ﻿
 Console.WriteLine("Программа для определения числа");
 double number = Convert.ToDouble(Console.ReadLine());
-//double result = number/2;
 if (number%2==0)
 {
     Console.WriteLine("Чётное");
